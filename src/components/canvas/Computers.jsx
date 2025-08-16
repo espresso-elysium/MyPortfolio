f6215837af98766539afc31196ffc6137e58cd38
espresso-elysium <espressoelysium@gmail.com>
2025-08-16 17:18:35 +0530

    Switch models to optimized Draco GLB

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,8 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene-draco.glb");
+
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
