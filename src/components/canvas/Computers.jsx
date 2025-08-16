8b4f9614a66128d0bcbe90083dddbba58ee76f79
espresso-elysium <espressoelysium@gmail.com>
2025-08-16 17:58:28 +0530

    Optimized GLTF models with Draco compression

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,19 +3,21 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/desktop_pc/scene-draco.glb");
+  const computer = useGLTF("/desktop_pc/scene-meshopt.glb");
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <spotLight>
+      <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
-        castShadow shadow-mapSize={1024}
-      </spotLight>
+        castShadow
+        shadow-mapSize={1024}
+      />
+
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
