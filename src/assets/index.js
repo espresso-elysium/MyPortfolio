786c3f81e161af93eb2386aa6377964a199110fd
espresso-elysium <espressoelysium@gmail.com>
2025-08-16 16:47:48 +0530

    Optimized images

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,28 +20,18 @@ import lp from "./lp.jpg"
 
 import linkedin from "./linkedin.png"
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
+
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
+
 import threejs from "./tech/threejs.svg";
 
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
   logo,
@@ -58,9 +48,9 @@ export {
   menu,
   close,
   css,
-  docker,
+ 
   solidity,
-  figma,
+
   git,
   html,
   javascript,
@@ -68,19 +58,16 @@ export {
   mongodb,
   nodejs,
   reactjs,
-  redux,
+
   tailwind,
-  typescript,
+  
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
+ 
+
   lp,
-  jobit,
+
   express,
-  tripguide,
+
   gmail,
   linkedin
 };
